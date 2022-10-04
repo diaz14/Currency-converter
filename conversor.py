0d4a6f37b9e7e82d91ce59ff8204d79ac2e8336a
Alonso Díaz <alonso-diaz@outlook.com>
2022-10-04 16:28:24 -0500

    Test: testing improvements

--- conversor.py
+++ conversor.py
@@ -1,6 +1,6 @@
 peso = input("¿Cuántos pesos mexicanos tienes?: ")
 peso = float(peso)
 valor_dolar = 19.97
-valor_dolar = peso/valor_dolar
+valor_dolar = str(peso/valor_dolar)
 valor_dolar = round(valor_dolar, 2)
-print str("Tienes $ " + valor_dolar + "dólares")
+print ("Tienes $ " + valor_dolar + "dólares")
