6317af4b750d5db82110fc22abb5c94a0259773f
Alonso Díaz <alonso-diaz@outlook.com>
2022-10-04 15:52:59 -0500

    Test: adding algorithm

--- conversor.py
+++ conversor.py
@@ -1,1 +1,6 @@
-moneda = 
+pesos = input("¿Cuántos pesos colombianos tienes?: ")
+pesos = float(pesos)
+valor_dolar = 3875
+dolares = pesos / valor_dolar
+dolares = str(dolares)
+print("Tienes $" + dolares + " dolares")
